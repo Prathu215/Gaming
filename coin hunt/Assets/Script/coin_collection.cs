d5e407ca36b6428795342cde4eda4f343aa229d0
prathyusha <prathyusha.system@gmail.com>
2018-01-13 16:46:57 -0500

    coin hunt

---
+++ coin hunt/Assets/Script/coin_collection.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class coin_collection : MonoBehaviour {
+
+	int score ;
+	// Use this for initialization
+	void Start () {
+		Cursor.visible = false;
+		Time.timeScale = 1;
+	}
+	
+	
+	void OnTriggerEnter (Collider other) {
+		if (other.tag == "coin") {
+			score += 1;
+			Destroy(other.gameObject);
+		}
+		
+	}
+	
+	void OnGUI() {
+		GUIStyle myStyle = new GUIStyle();
+		myStyle.fontSize = 30;
+		myStyle.normal.textColor = Color.red;
+		myStyle.hover.textColor = Color.red;
+		GUI.Label (new Rect (50,10, 50, 50), "Score " + score, myStyle);
+		if (score == 50 ) {
+		Application.LoadLevel(2);
+		}
+	}
+}
---
+++ coin hunt/Assets/Script/timer.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class timer : MonoBehaviour 
+{
+	private string timedis;
+	private float ttimer = 180.0f;
+	private int min;
+	private int hou;
+	
+	public bool startime = false;
+	void Start() 
+	{
+	}
+	void Update () 
+	{   
+
+		if(startime == true)
+		{
+
+			ttimer -= Time.deltaTime;
+			timedis = (Mathf.Floor(ttimer).ToString());
+
+		}
+	}
+	void OnGUI(){
+		GUIStyle Label2 = new GUIStyle(GUI.skin.GetStyle("label"));
+		Label2.fontSize = 27;
+		Label2.normal.textColor = Color.red ;
+
+		GUI.Label(new Rect(50,40,200,200), "Time Left : " + timedis, Label2);
+		if(ttimer <= 1){
+			Application.LoadLevel("gameover");
+		}
+	}
+}
